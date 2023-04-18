d1b8ef62cec7dff0a2f80de5c437f1775b536b94
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-04-18 11:16:30 +0700

    chore: update requirements.txt

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -8,12 +8,12 @@ import uuid
 router = APIRouter(prefix="/upload", tags=["upload"], responses={
                    404: {"description": "Not found"}})
 
+
 @router.post("/")
 async def upload_slip(file: UploadFile = File(...)):
   if not os.path.exists("images"):
     os.makedirs("images")
   name = str(uuid.uuid4())
   with open(f"images/{name}.png", "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+    shutil.copyfileobj(file.file, buffer)
   return {"filename": f'images/{name}.png'}
-  
